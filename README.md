d3c145271567254832024912db60ca0946452803
Julius Busecke <you@example.com>
2025-08-05 17:09:58 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# LNDAS-icechunk
+
+## Dependency management
+
+This repo uses [uv](https://docs.astral.sh/uv/) as package/project manager.
+
+
+### Running Jupyter Notebooks on the NASA VEDA hub
+
+To reproduce results in the notebooks, you need to build a custom kernel with 
+
+```
+uv sync
+uv run bash
+python -m ipykernel install --user --name=lndasenv --display-name="LNDAS-VENV"
+```
+
+Then select the "LNDAS-VENV" kernel on the upper right corner drop-down in your notebook (you might have to refresh the browser to see it). 
+
+### Running scripts
+
+You can run the scripts with 
+```
+uv run <scriptname>
+```
---
+++ scripts/icechunk-conversion.py
@@ -0,0 +1,65 @@
+import fsspec
+from virtualizarr import open_virtual_mfdataset
+from virtualizarr.parsers import HDFParser
+from virtualizarr.registry import ObjectStoreRegistry
+
+import obstore
+import icechunk
+
+## Find Files of interest
+
+data_dir = "s3://nasa-waterinsight/NLDAS3/forcing/daily/"
+# change these as needed.
+store_bucket = "nasa-veda-scratch"
+store_prefix = "jbusecke/NLDAS3/"
+
+# Use fsspec to list files in the S3 bucket
+fs = fsspec.filesystem("s3", anon=True)
+files = fs.glob(data_dir + "**/*.nc")
+print(f"{len(files)} found")
+
+## Produce a virtual dataset from the list of files
+bucket = "s3://nasa-waterinsight"
+store = obstore.store.from_url(bucket, region="us-west-2", skip_signature=True)
+
+registry = ObjectStoreRegistry({bucket: store})
+
+parser = HDFParser()
+
+urls = ["s3://" + file for file in files]
+vds = open_virtual_mfdataset(urls, parser=parser, registry=registry, parallel="lithops")
+
+print(f"Virtual Dataset: {vds}")
+
+## Write (commit) the virtual dataset into icechunk
+storage = icechunk.s3_storage(
+    bucket=store_bucket,
+    prefix=store_prefix,
+    anonymous=False,
+    from_env=True,
+)
+
+config = icechunk.RepositoryConfig.default()
+config.set_virtual_chunk_container(
+    icechunk.VirtualChunkContainer(
+        "s3://nasa-waterinsight/NLDAS3/forcing/daily/202201/",
+        icechunk.s3_store(region="us-west-2"),
+    )
+)
+
+virtual_credentials = icechunk.containers_credentials(
+    {
+        "s3://nasa-waterinsight/NLDAS3/forcing/daily/202201/": icechunk.s3_anonymous_credentials()
+    }
+)
+
+repo = icechunk.Repository.open_or_create(
+    storage=storage,
+    config=config,
+    authorize_virtual_chunk_access=virtual_credentials,
+)
+
+session = repo.writable_session("main")
+vds.vz.to_icechunk(session.store)
+session.commit("First Try full dataset")
+print("DONE")
